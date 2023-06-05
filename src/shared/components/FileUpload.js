1f36ac8ecf2244d9764f9471a0b4f62f47572f8d
gavand-sagar <dipesh@trainitsolutions.com>
2023-06-06 00:52:38 +0530

    drag and drop

--- src/shared/components/FileUpload.js
+++ src/shared/components/FileUpload.js
@@ -24,12 +24,50 @@ export default function FileUpload({ onUpload }) {
             setUploadState('')
         })
     }
+
+    function dropHandler(ev) {
+        console.log("File(s) dropped");
+
+        // Prevent default behavior (Prevent file from being opened)
+        ev.preventDefault();
+
+        if (ev.dataTransfer.items) {
+            // Use DataTransferItemList interface to access the file(s)
+            [...ev.dataTransfer.items].forEach((item, i) => {
+                // If dropped items aren't files, reject them
+                if (item.kind === "file") {
+                    const file = item.getAsFile();
+                    uploadFile(file)
+                    console.log(`… file[${i}].name = ${file.name}`);
+                }
+            });
+        } else {
+            // Use DataTransfer interface to access the file(s)
+            [...ev.dataTransfer.files].forEach((file, i) => {
+                console.log(`… file[${i}].name = ${file.name}`);
+            });
+        }
+    }
+
+    function dragOverHandler(ev) {
+        console.log("File(s) in drop zone");
+
+        // Prevent default behavior (Prevent file from being opened)
+        ev.preventDefault();
+    }
+
+
+
     return (
-        <div className='file-upload-component'>
+        <div onDragOver={dragOverHandler} onDrop={dropHandler} className='file-upload-component'>
             <label htmlFor='app-file-upload'>
                 <CameraAltIcon />
             </label>
-            
+            <br />
+            {uploadState != 'Uploading...' && fileName == ''
+                ? "Drop Files or Click here To upload"
+                : ""
+            }
             <input id="app-file-upload" onChange={upload} label="Song Image" type='file' />
             <br />
             <br />
@@ -37,7 +75,7 @@ export default function FileUpload({ onUpload }) {
                 uploadState
             }
             {
-                uploadState != 'Uploading...' &&  fileName && <img src={'http://localhost:3001/image/' + fileName} />
+                uploadState != 'Uploading...' && fileName && <img src={'http://localhost:3001/image/' + fileName} />
             }
 
         </div>
